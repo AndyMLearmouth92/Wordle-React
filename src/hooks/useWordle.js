f9e8a869dd873b9d5d0b0e7b2d073d6ed2dee5eb
Andy Learmouth <andymlearmouth@hotmail.com>
2023-05-20 09:04:33 +0100

    Function adding formatted guess to guesses and history states and increments the turn state by one and resets current guess.

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const useWordle = (solution) => {
     const [turn, setTurn] = useState(0) 
     const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([]) // each guess is an array
+    const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
     const [history, setHistory] = useState([]) // each guess is a string
     const [isCorrect, setIsCorrect] = useState(false)
 
@@ -36,8 +36,22 @@ const useWordle = (solution) => {
     //Add a new guess to the guesses state
     //Update the isCorrect state if the guess is correct
     // add one to the turn state
-    const addNewGuess = () => {
-
+    const addNewGuess = (formattedGuess) => {
+        if(currentGuess === solution){
+            setIsCorrect(true)
+        }
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses]
+            newGuesses[turn] = formattedGuess
+            return newGuesses
+        })
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess]
+        })
+        setTurn((prevTurn) => {
+            return prevTurn +1
+        })
+        setCurrentGuess('')
     }
 
     //handle keyup event and track current guess
@@ -60,8 +74,9 @@ const useWordle = (solution) => {
                 return
             }
             const formatted = formatGuess()
-            console.log(formatted)
+            addNewGuess(formatted)
         }
+
         if(key === 'Backspace'){
             setCurrentGuess((prev) => {
                 return prev.slice(0, -1)
